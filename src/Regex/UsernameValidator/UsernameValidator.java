b22bbcb03d1a438b7a3845786eef94b0cd582566
mohit-on-github <official.mohitkr19@gmail.com>
2022-12-22 14:29:41 +0530

    Regex problem

---
+++ src/Regex/UsernameValidator/UsernameValidator.java
@@ -0,0 +1,8 @@
+package UsernameValidator;
+
+public class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression ="[A-Za-z]{1}[\\w_]{7,29}";
+}
